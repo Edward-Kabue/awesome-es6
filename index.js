e1254fd4b179fe48d7f798c9e9a725229fbc86b6
Edward Kabue <ekabue0@gmail.com>
2022-03-21 18:27:03 +0300

    Update styling

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-import { DateTime } from './node_modules/luxon/src/luxon.js';
 import './modules/book.js';
 import './modules/library.js';
 import showBlock from './modules/navbar.js';
 
-const time = document.getElementById('date');
-setInterval(() => {
-  time.innerHTML = DateTime.now().toLocaleString(
-    DateTime.DATETIME_MED_WITH_SECONDS
-  );
-}, 1000);
-
+const displayTime = () => {
+  document.getElementById('date').innerText = new Date().toLocaleString();
+  setTimeout(displayTime, 1000);
+};
+displayTime();
 const links = document.querySelectorAll('.links');
 links.forEach((element) => {
   element.addEventListener('click', function () {
