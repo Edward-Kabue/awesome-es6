8a335e9ff21b7e2d97cbad61ba18a8976cd68ab9
Edward Kabue <ekabue0@gmail.com>
2022-03-21 12:19:06 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+## Awesome books
+
+Developed a  that displays a list of books. The user can add books by providing a book title and an author. The user can also delete a certain book from the list by clicking the delete button.
+
+
+## Built With
+
+- html
+- css/sass
+- Javascript
+
+
+ 
+## To get a local copy up and running follow these simple example steps.
+1. Under the repository name, click the Clone or download green button.
+2. Copy the URL given by clicking the clipboard button
+3. Open a terminal window in your local machine and change the current directory to the one you want the clone directory to be made.
+4. Type git clone and the paste the URL you previusly copied to the clipboard
+5. Change the current directory to the newly created folder
+
+
+## Authors
+
+### Author 1
+
+- GitHub: [@Edward-Kabue](https://github.com/Edward-Kabue)
+- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+
+### Author 2
+- Github: [LionRouge](https://github.com/LionRouge1)
+- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ main.js
@@ -0,0 +1,135 @@
+/* eslint-disable  */
+class Books {
+  constructor(title = '', author = '') {
+    this.title = title;
+    this.author = author;
+    this.books = localStorage.getItem('books') !== null
+      ? JSON.parse(localStorage.getItem('books'))
+      : '';
+  }
+
+  saveBook(BTSave = this.books) {
+    const storeBook = JSON.stringify(BTSave);
+    localStorage.setItem('books', storeBook);
+  }
+
+  addBook() {
+    if (this.books === '' || this.books.length <= 0) {
+      this.books = [
+        {
+          Id: 1,
+          Btitle: this.title,
+          Bauthor: this.author,
+        },
+      ];
+      this.saveBook();
+    } else {
+      const lastId = this.books[this.books.length - 1].Id + 1;
+      const book = {
+        Id: lastId,
+        Btitle: this.title,
+        Bauthor: this.author,
+      };
+      this.books.push(book);
+      this.saveBook();
+    }
+  }
+
+  removeBook(bookId) {
+    const remove = this.books.filter((book) => book.Id !== Number(bookId));
+    this.saveBook(remove);
+  }
+
+  static displayBook(book) {
+    const list = document.querySelector('#form-list');
+    const row = document.createElement('tr');
+    row.innerHTML = ` <td>${book.Btitle}</td> 
+    <td>${book.Bauthor}</td> <td>
+    <button type="submit" id="${book.Id}" class="btn">Remove</button></td> `;
+    list.appendChild(row);
+  }
+}
+
+//  display all the books;
+
+if (localStorage.getItem('books') !== null) {
+  const books = JSON.parse(localStorage.getItem('books'));
+  books.forEach((element) => {
+    Books.displayBook(element);
+  });
+}
+
+// add book function
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  if (title.value === '' || author.value === '') {
+    const error = document.getElementById('error');
+    error.textContent = 'Title and Author fields must be filled out';
+    error.style.color = 'red';
+    e.preventDefault();
+  } else {
+    const addBk = new Books(title.value, author.value);
+    addBk.addBook();
+  }
+});
+
+// Remove book function
+
+function removeBk() {
+  const Book = new Books();
+  Book.removeBook(this.id);
+  this.parentNode.parentNode.remove();
+}
+
+const btn = document.querySelectorAll('.btn');
+btn.forEach((element) => {
+  element.addEventListener('click', removeBk);
+});
+
+// navbar section
+
+const links = document.querySelectorAll('.links');
+
+function showBlock(e) {
+  const sectionList = document.getElementById('list');
+  const sectionAdd = document.getElementById('add_book');
+  const sectionContact = document.getElementById('contact');
+  switch (e) {
+    case 'list-link':
+      sectionList.style.display = 'block';
+      sectionAdd.style.display = 'none';
+      sectionContact.style.display = 'none';
+      break;
+
+    case 'add-link':
+      sectionAdd.style.display = 'block';
+      sectionList.style.display = 'none';
+      sectionContact.style.display = 'none';
+      break;
+
+    case 'contact-link':
+      sectionContact.style.display = 'flex';
+      sectionContact.style.visibility = 'visible';
+      sectionAdd.style.display = 'none';
+      sectionList.style.display = 'none';
+      break;
+
+    default:
+      sectionList.style.display = 'block';
+      sectionAdd.style.display = 'none';
+      sectionContact.style.display = 'none';
+      break;
+  }
+}
+
+links.forEach((element) => {
+  element.addEventListener('click', function () {
+    showBlock(element.id);
+    const current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(' active', '');
+    this.className += ' active';
+  });
+});
